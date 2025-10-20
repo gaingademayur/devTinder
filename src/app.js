624947beb6eedd03534f569caf0699411fc76554
Mayur <gaingademayur@gmail.com>
2025-10-20 22:44:15 +0530

    schema level data sanitization

--- src/app.js
+++ src/app.js
@@ -17,9 +17,79 @@ app.post("/signup", async (req, res) => {
     }
 });
 
-// app.use("/user", userAuth, (req, res) => {
-//     res.send("user api");
-// });
+app.get("/user", async (req, res) => {
+    try {
+        const userId = req.body._id;
+
+        const user = await User.findOne({_id: userId});
+        if(!user) {
+            res.status(404).send("no user found");
+        }
+        else {
+            res.send(user)
+        }
+    } catch(ex) {
+        res.send("something went wrong");
+    }
+    res.send("user api");
+});
+
+app.get("/feed", async (req, res) => {
+    try {
+
+        const user = await User.find();
+        if(user.length == 0) {
+            res.status(404).send("no user found");
+        }
+        else {
+            res.send(user)
+        }
+    } catch(ex) {
+        res.send("something went wrong");
+    }
+    res.send("user api");
+});
+
+app.delete("/user", async (req, res) => {
+    try {
+        const userId = req.body._id;
+
+        const user = await User.findByIdAndDelete({_id: userId});
+        if(!user) {
+            res.status(404).send("no user found");
+        }
+        else {
+            res.send(user)
+        }
+    } catch(ex) {
+        res.send("something went wrong");
+    }
+    res.send("user api");
+});
+
+app.patch("/update/:userId", async (req, res) => {
+    try {
+        const userId = req.params?.userId;
+
+        const ALLOWED_UPDATES = [
+            "age",
+            "firstName",
+            "lastName"
+        ];
+
+        const allowedData = Object.keys(req.body).every((key) => ALLOWED_UPDATES.includes(key));
+
+        if(!allowedData) {
+            throw new Error("update not allowed");
+        }
+
+        const user = await User.findByIdAndUpdate(userId, req.body, {runValidators: true});
+        user.save();
+        res.send("user updated success");
+    } catch(err) {
+        res.status(400).send(err.message);
+    }
+});
 
 app.listen(3000, () => {
     console.log("server is successfully listening on port 3000.");
--- src/models/user.js
+++ src/models/user.js
@@ -2,21 +2,32 @@ const mongoose = require("mongoose");
 
 const userScheme = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 2,
+        maxLength: 20
     },
     lastName: {
-        type: String
+        type: String,
+        minLength: 2,
+        maxLength: 20
     },
     emailId: {
-        type: String
+        type: String,
+        unique: true,
+        lowercase: true
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18
     },
     password: {
-        type: String
+        type: String,
+        minLength: 6,
+        maxLength: 10
     }
-});
+},
+{timestamps: true});
 
 const userModel = mongoose.model('User', userScheme);
 
